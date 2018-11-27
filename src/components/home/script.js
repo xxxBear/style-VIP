75721ff4bc4a27d5d1080c43a35e6575cb7a9863
Bear <564809229@qq.com>
2018-11-27 15:57:08 +0800

    fifth

--- src/components/home/script.js
+++ src/components/home/script.js
@@ -5,22 +5,68 @@ export default {
     };
   },
   created() {
-    this.fetchStoreList();
+    // this.fetchStoreList();
   },
   methods: {
-    getStoreList() {
-      this.$http.post('/ecp.crm.store.getlist', {
-          params: {
-            pagesize: 2,
-            pageno: 1
-          }
-        })
-        .then(res => {
+    // 请求参数加密
+    getParams(params, _functionname, _async) {
+      var sign = '';
+      // 参数对象拼接字符串
+      var str = '';
+      for (k in params) {
+        var v = k + params[k];
+        str += v;
+      }
+
+      var _functionname = _functionname;
+      var _async = _async || false;
+      var _appkey = '559ab76da1fe45052055da60';
+      var _sessionkey = '559ab76da1fe45052055da62';
+      // 获取当前时间戳，精确到毫秒
+      var _timestamp = new Date().getTime();
+      // var _timestamp = new Date().valueOf();
+      var _version = '1.0';
+      var secretkey = '559ab76da1fe45052055da61';
+
+      sign = (md5(secretkey +
+        '_appkey' + _appkey +
+        '_async' + _async +
+        '_functionname' + _functionname +
+        '_sessionkey' + _sessionkey +
+        '_version' + _version +
+        '_timestamp' + _timestamp +
+        // 请求参数拼串
+        str +
+        secretkey)).toLocaleLowerCase();
+      return newParams = {
+        params,
+        "_appkey": '559ab76da1fe45052055da60',
+        "_sessionkey": '559ab76da1fe45052055da62',
+        "_functionname": _functionname,
+        "_version": '1.0',
+        "_async": _async,
+        "_timestamp": _timestamp,
+        "sign": sign
+      }
+    },
+    async getStoreList() {
+      this.$http.post('ecp.crm.store.getlist', getParams({
+          pagesize: 2,
+          pageno: 1
+        }, getlist, false))
+        .then((res) => {
           console.log(res);
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
         })
+
+      // const ret = this.$http.post('ecp.crm.store.getlist', getParams({
+      //   pageno: 1,
+      //   pagesize: 2
+      // }, getlist, false));
+
+      // console.log(ret);
     },
     sortDefault() {
       // console.log("默认排序");
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App';
 import router from './router';
 import axios from 'axios';
 import MuseUI from 'muse-ui';
+import md5 from 'js-md5';
 
 import 'muse-ui/dist/muse-ui.css';
 // 全局样式导入
@@ -18,20 +19,21 @@ Vue.use(MuseUI);
 Vue.component("footernav", FooterNav);
 Vue.component("backtotop", BacktoTop);
 
+Vue.prototype.$md5 = md5;
 Vue.prototype.$http = axios;
 axios.defaults.baseURL = 'http://open.ecp100.com/router';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 // 请求拦截器
-axios.interceptors.request.use((config) => {
-  if (config.methods === 'post') {
-    // 参数序列化
-    config.data = qs.stringify(config.data);
-  }
-  return config
-}), (error) => {
-  return error;
-}
+// axios.interceptors.request.use((config) => {
+//   if (config.methods === 'post') {
+//     // 参数序列化
+//     // config.data = qs.stringify(config.data);
+//   }
+//   return config
+// }), (error) => {
+//   return error;
+// };
 
 Vue.config.productionTip = false;
 
